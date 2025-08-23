dce765d0c1be5d7835621d2f5bdfe253d635f709
Wooju <woojukwon8@gmail.com>
2025-08-23 20:18:19 +0900

    design: 경로 선 디자인 수정

--- frontend/lib/presentation/screens/route_view_screen.dart
+++ frontend/lib/presentation/screens/route_view_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:frontend/providers/route_provider.dart';
 import 'package:kakao_map_sdk/kakao_map_sdk.dart';
 
@@ -19,33 +20,137 @@ class _RouteViewScreenState extends ConsumerState<RouteViewScreen> {
     final routeState = ref.watch(routeProvider);
 
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('경로 보기'),
-        backgroundColor: Colors.white,
-        foregroundColor: Colors.black,
-        elevation: 0,
-      ),
-      body: routeState.when(
-        data: (routeResponse) {
-          if (routeResponse == null) {
-            return const Center(child: Text('경로 데이터가 없습니다.'));
-          }
+      body: Stack(
+        children: [
+          routeState.when(
+            data: (routeResponse) {
+              if (routeResponse == null) {
+                return const Center(child: Text('경로 데이터가 없습니다.'));
+              }
 
-          // 경로 좌표를 LatLng 리스트로 변환
-          _routePoints = routeResponse.pathPoints
-              .map(
-                (point) => LatLng(point[1], point[0]),
-              ) // [경도, 위도] -> LatLng(위도, 경도)
-              .toList();
+              // 경로 좌표를 LatLng 리스트로 변환
+              _routePoints = routeResponse.pathPoints
+                  .map(
+                    (point) => LatLng(point[1], point[0]),
+                  ) // [경도, 위도] -> LatLng(위도, 경도)
+                  .toList();
 
-          if (_routePoints.isEmpty) {
-            return const Center(child: Text('경로 좌표가 없습니다.'));
-          }
+              if (_routePoints.isEmpty) {
+                return const Center(child: Text('경로 좌표가 없습니다.'));
+              }
 
-          return _buildMap();
-        },
-        loading: () => const Center(child: CircularProgressIndicator()),
-        error: (error, stack) => Center(child: Text('오류: $error')),
+              return _buildMap();
+            },
+            loading: () => const Center(child: CircularProgressIndicator()),
+            error: (error, stack) => Center(child: Text('오류: $error')),
+          ),
+          Positioned(
+            top: 70,
+            left: 40,
+            right: 40,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                  child: SvgPicture.asset(
+                    'assets/icons/back.svg',
+                    width: 30,
+                    height: 30,
+                  ),
+                ),
+                Container(
+                  width: 300,
+                  height: 60,
+                  decoration: ShapeDecoration(
+                    color: Colors.white,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    shadows: [
+                      BoxShadow(
+                        color: Color(0x3F9F9F9F),
+                        blurRadius: 3.84,
+                        offset: Offset(-0.96, 0),
+                        spreadRadius: 0,
+                      ),
+                      BoxShadow(
+                        color: Color(0x3F787878),
+                        blurRadius: 3.84,
+                        offset: Offset(0.96, 0.96),
+                        spreadRadius: 0,
+                      ),
+                    ],
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        '부산역',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: const Color(0xFF374151),
+                          fontSize: 20,
+                          fontFamily: 'Pretendard',
+                          fontWeight: FontWeight.w500,
+                          height: 1.29,
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.symmetric(horizontal: 10),
+                        child: SvgPicture.asset(
+                          'assets/icons/arrow.svg',
+                          width: 16,
+                          height: 16,
+                        ),
+                      ),
+                      Text(
+                        '서면교차로',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: const Color(0xFF374151),
+                          fontSize: 20,
+                          fontFamily: 'Pretendard',
+                          fontWeight: FontWeight.w500,
+                          height: 1.29,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Positioned(
+            bottom: 200,
+            left: 0,
+            child: Row(
+              children: [
+                RouteName(routeName: '쉬운 길 추천'),
+                RouteName(routeName: '내비 추천'),
+                RouteName(routeName: '큰길 우선'),
+              ],
+            ),
+          ),
+          Positioned(
+            bottom: 0,
+            child: Container(
+              width: MediaQuery.sizeOf(context).width,
+              height: 270,
+              decoration: ShapeDecoration(
+                color: Colors.white,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(23.06),
+                    topRight: Radius.circular(23.06),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
@@ -83,23 +188,25 @@ class _RouteViewScreenState extends ConsumerState<RouteViewScreen> {
     _mapController!.routeLayer.addRoute(
       _routePoints,
       RouteStyle(
-        const Color(0xFFFF5930), // 경로 색상 (주황색)
-        8.0, // 경로 굵기
+        const Color(0xFFFF9E2B), // 경로 색상
+        12.0, // 경로 굵기
+        strokeColor: Color(0xFFFFFFFF),
+        strokeWidth: 4,
       ),
     );
 
     // 시작점 POI 추가
     _mapController!.labelLayer.addPoi(
       _routePoints.first,
-      style: PoiStyle(),
-      text: '출발',
+      style: PoiStyle(
+        icon: KImage.fromAsset("assets/icons/my_location.png", 30, 30),
+      ),
     );
 
     // 도착점 POI 추가
     _mapController!.labelLayer.addPoi(
       _routePoints.last,
-      style: PoiStyle(),
-      text: '도착',
+      style: PoiStyle(icon: KImage.fromAsset("assets/icons/pin.png", 21, 28)),
     );
   }
 
@@ -228,5 +335,40 @@ class _RouteViewScreenState extends ConsumerState<RouteViewScreen> {
     Future.delayed(const Duration(milliseconds: 300), () {
       _mapController?.moveCamera(CameraUpdate.zoomTo(zoomLevel));
     });
+  }
+}
+
+class RouteName extends StatelessWidget {
+  const RouteName({super.key, required this.routeName});
+  final String routeName;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.sizeOf(context).width / 3,
+      height: 140,
+      decoration: ShapeDecoration(
+        color: Colors.white,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(12),
+            topRight: Radius.circular(12),
+          ),
+        ),
+      ),
+      child: Column(
+        children: [
+          SizedBox(height: 24),
+          Text(
+            routeName,
+            style: TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.bold,
+              color: Color(0xFFFF792B),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
